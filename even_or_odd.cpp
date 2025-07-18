d776c6774d1a5cbca8b723e84d20b10c3bf19e2d
vinay-k-o-l-i <vinayramlukoli@gmail.com>
2025-07-18 22:28:31 +0530

    Create even_or_odd.cpp

---
+++ even_or_odd.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() {
+int number;
+cout<<"Enter a number :";
+cin>>number;
+if(number%2==0)
+{
+    cout<<"Even number"<<endl;
+}
+else
+{
+    cout<<"odd number"<<endl;
+
+}
+}
+/*PS C:\Users\CL301_11\Desktop\exp02>                                                                                                                  > cd "c:\Users\CL301_11\Desktop\exp02\" ; if ($?) { g++ tempCodeRunnerFile.cpp -o tempCodeRunnerFile } ; if ($?) { .\tempCodeRunnerFile }
+Enter a number :22
+Even number*/
